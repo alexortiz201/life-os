b8433e8d66b190d46616528d7b3a90b5c6c8def5
Alexander Ortiz <alexanderortiz.201@gmail.com>
2026-02-11 11:40:00 -0500

    Clean up

---
+++ life-os/src/platform/pipeline/stage/stage.types.ts
@@ -0,0 +1,32 @@
+import * as E from "fp-ts/Either";
+
+import type {
+  PipelineStageErrorSeverity,
+  PipelineStageName,
+} from "#/rna/pipeline/ingestion/ingestion.types";
+import { PipelineStageError } from "#/platform/pipeline/pipeline.types";
+
+export type StageLeft<
+  TEnv,
+  TStage extends PipelineStageName = PipelineStageName,
+  TCode extends string = string,
+> = {
+  env: TEnv;
+  error: PipelineStageError<TStage, PipelineStageErrorSeverity, TCode>;
+};
+
+export type PipelineStageFn<
+  TEnvIn,
+  TStage extends PipelineStageName,
+  TCode extends string,
+  TEnvOut = TEnvIn,
+> = (env: TEnvIn) => E.Either<StageLeft<TEnvOut, TStage, TCode>, TEnvOut>;
+
+export type StageLeftHalt<
+  TEnv,
+  TStage extends PipelineStageName,
+  TCode extends string,
+> = {
+  env: TEnv;
+  error: PipelineStageError<TStage, "HALT", TCode>;
+};
